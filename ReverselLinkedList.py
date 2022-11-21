103d1d0f3199fccb4c7d4302d8fffb56ef540721
uhunmwa <uhunmwa@uwindsor.ca>
2022-11-20 23:56:52 -0500

    ReverseLinkList.py

---
+++ ReverselLinkedList.py
@@ -0,0 +1,23 @@
+#Input: A Linked list of  integer numbers 
+#output: A reverse linked list
+
+
+
+def reverseLinkedList(self,head:Optional[ListNode])->Optional[ListNode]:
+	prev = None
+	curr = head
+
+#This while loop condition allows us to stay within the linked list and lets  us know
+#when we get to the ened because i will be null
+
+
+#how this works needs a bit more than documentation to explain
+#diagram will be best
+	while head:
+		next = curr.next 
+		curr.next = prev
+		prev = head
+		
+
+
+
