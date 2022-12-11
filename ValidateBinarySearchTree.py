f034cc84925581839f67a0bfd8947d61a9b1d378
uhunmwa <uhunmwa@uwindsor.ca>
2022-12-11 15:19:02 -0500

    Validate Binary Search Tree

---
+++ ValidateBinarySearchTree.py
@@ -0,0 +1,20 @@
+#problem: Valdiate binary search tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+
+class Solution:
+	def isValidBST(self,root:Optional[TreeNode])->bool:
+		def valido(node,left,right):
+			if node is None:
+				return True
+			if not(node.val>left and node.val<right):
+				return false
+			return valid(node.left,left,node.val) and valid(node.right,node.val,right)
+		return valid(root,float("-inf"),float("inf"))
